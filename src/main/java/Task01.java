a98accaaef5c088caea333fff9d57cebf9aba6a3
aneethajude <aneetha.joseph@gmail.com>
2023-01-27 12:12:57 +0800

    Task02 added

--- src/main/java/Task01.java
+++ src/main/java/Task01.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Task01 {
     public static String removePunctuations(String source) {
@@ -14,34 +15,57 @@ public class Task01 {
     }
 
     public static void main(String[] args){
+
+        //Variables declaration & initialisation
         String source=null;
         String str = args[0];
         System.out.println(str);
         String line;
+        String updatedString;
+        int count = 0, maxCount = 0;    
+        ArrayList<String> words = new ArrayList<String>();    
+        
+        // Coding begins
         Path path = Paths.get(str);
         File file = path.toFile();
-       
-
+        
         try {
             // Open the given file
             
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
-       
-            while(null!=(line=br.readLine())){
-               line.toUpperCase();
-               String updatedString  = removePunctuations(line);
-               System.out.println(updatedString);
-                
-                }
-            
-        br.close();
-        fr.close();
-        
 
-            }catch(IOException e){
-                System.out.println("Something went wrong");
+       //Stripping off punctuations & ignoring case of words
+            while(null!=(line=br.readLine())){
+              // line.toUpperCase();
+              // updatedString  = removePunctuations(line);
+               //System.out.println(updatedString);
+              // updatedString.toLowerCase();    
+               String string[] = line.toLowerCase().split("([,.:!-{}'\\s]+) ");   
+               for (int i=0;i<string.length;i++){
+               System.out.println(string[i]) ;}
+                //Adding all words generated in previous step into words    
+                for(String s : string){    
+                words.add(s); 
+                }    
+                for(int i = 0; i < words.size(); i++){    
+                count = 1;    
+                //Count each word in the file and store it in variable count    
+                for(int j = i+1; j < words.size(); j++){    
+                    if(words.get(i).equals(words.get(j))){    
+                        count++;    
+                    }     
+                }    
+                    }
+            
+                //closing the readers  
+                        br.close();
+                        fr.close();
+
             }
+        }catch(IOException e){
+                System.out.println("Something went wrong");
+                    }
            
     
 
---
+++ src/main/java/Task02.java
@@ -0,0 +1,8 @@
+
+
+public class Task02 {
+    public static void main(String[] args){
+        
+    }
+    
+}
